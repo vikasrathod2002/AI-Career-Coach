f3b8ce06b938a03c2c68ba0b7fdbe3dcda5c1796
vikasrathod2002 <vikassrathod2002@gmail.com>
2025-03-28 17:52:16 +0530

    commit change

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,14 @@
-const config = {
+/*const config = {
   plugins: ["@tailwindcss/postcss"],
+};
+
+export default config; /*
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
 };
 
 export default config;
